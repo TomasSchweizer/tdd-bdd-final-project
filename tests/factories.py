1ac73f21f1019d33cb775c47aaa32ca041c16c2c
TomasSchweizer <schweizertomas@protonmail.com>
2025-03-26 11:28:24 -0400

    Added a ProdcutFactory to create test data.

--- tests/factories.py
+++ tests/factories.py
@@ -27,8 +27,34 @@ class ProductFactory(factory.Factory):
 
     class Meta:
         """Maps factory to data model"""
-
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )    
+    description = factory.Faker("catch_phrase")
+    price = FuzzyDecimal(0.01, 10000.0, precision=2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
